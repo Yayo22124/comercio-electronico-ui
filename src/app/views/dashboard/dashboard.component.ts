39ca416bb158b2efb2c67402eb1d4a5124f08c13
Haziel Ortiz <eortiz@inncol.com.mx>
2024-08-14 10:26:36 -0600

    fix(chart): Remove random data

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -35,7 +35,6 @@ export class DashboardComponent implements OnInit, OnDestroy {
   private oneDay = 24 * 3600 * 1000;
   private now!: Date;
   private value!: number;
-  private data!: DataT[];
   private timer: any;
 
   constructor() {}
@@ -45,98 +44,10 @@ export class DashboardComponent implements OnInit, OnDestroy {
     this.getTotalPedidos();
     this.getTotalProductosVendidos();
     this.getVentas();
-    // generate some random testing data:
-    this.data = [];
-    this.now = new Date(1997, 9, 3);
-    this.value = Math.random() * 1000;
-
-    for (let i = 0; i < 1000; i++) {
-      this.data.push(this.randomData());
-    }
-
-    // initialize chart options:
-    this.options = {
-      title: {
-        text: 'Dynamic Data + Time Axis',
-      },
-      tooltip: {
-        trigger: 'axis',
-        formatter: (params: any) => {
-          params = params[0];
-          const date = new Date(params.name);
-          return (
-            date.getDate() +
-            '/' +
-            (date.getMonth() + 1) +
-            '/' +
-            date.getFullYear() +
-            ' : ' +
-            params.value[1]
-          );
-        },
-        axisPointer: {
-          animation: false,
-        },
-      },
-      xAxis: {
-        type: 'time',
-        splitLine: {
-          show: false,
-        },
-      },
-      yAxis: {
-        type: 'value',
-        boundaryGap: [0, '100%'],
-        splitLine: {
-          show: false,
-        },
-      },
-      series: [
-        {
-          name: 'Mocking Data',
-          type: 'line',
-          showSymbol: false,
-          data: this.data,
-        },
-      ],
-    };
-
-    // Mock dynamic data:
-    this.timer = setInterval(() => {
-      for (let i = 0; i < 5; i++) {
-        this.data.shift();
-        this.data.push(this.randomData());
-      }
-
-      // update series data:
-      this.updateOptions = {
-        series: [
-          {
-            data: this.data,
-          },
-        ],
-      };
-    }, 1000);
   }
 
   ngOnDestroy() {
     clearInterval(this.timer);
-  }
-
-  randomData(): DataT {
-    this.now = new Date(this.now.getTime() + this.oneDay);
-    this.value = this.value + Math.random() * 21 - 10;
-    return {
-      name: this.now.toString(),
-      value: [
-        [
-          this.now.getFullYear(),
-          this.now.getMonth() + 1,
-          this.now.getDate(),
-        ].join('/'),
-        Math.round(this.value),
-      ],
-    };
   }
 
   getClientesNuevos(): void {
@@ -248,7 +159,3 @@ export class DashboardComponent implements OnInit, OnDestroy {
   }
 }
 
-type DataT = {
-  name: string;
-  value: [string, number];
-};
